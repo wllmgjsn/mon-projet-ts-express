6c20d2bff68bcb13a1d3cffc20f6a2891de92190
wllmgjsn <william.gijsen@student.vinci.be>
2025-10-27 15:46:23 +0100

    EDIT book.model

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -1,9 +1,18 @@
-interface Book {
-    id : number,
-    title : string,
-    author : string,
-    year : number,
-    genre : string
-};
+interface Book{
+    id: number,
+    title: string,
+    author: string,
+    year: number,
+    genre: string
+}
 
-export default Book;
+interface FilterBook{
+    title?: string,
+    author?: string,
+    year?: number,
+    genre?:string
+}
+
+type NewBook = Omit<Book, "id">
+
+export type {Book, NewBook, FilterBook};
